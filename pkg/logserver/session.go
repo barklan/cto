43b94abcc42cfea493b58c9f1ec4a19bc28f1852
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-05 16:52:45 +0300

    feat(logserver): regular force cleaning of error records

--- pkg/logserver/session.go
+++ pkg/logserver/session.go
@@ -11,10 +11,10 @@ import (
 )
 
 func openSession(data *storage.Data) map[string]*SessionData {
-	knownErrors := make([]KnownError, 0)
-
 	sessionDataMap := map[string]*SessionData{}
+
 	for projectName := range data.Config.P {
+		knownErrors := make([]KnownError, 0)
 		sessionData := &SessionData{
 			KnownErrors:      knownErrors,
 			KnownErrorsMutex: sync.Mutex{},
@@ -25,6 +25,7 @@ func openSession(data *storage.Data) map[string]*SessionData {
 	if data.Config.Internal.Log.ClearOnRestart == true {
 		log.Println("clearing known errors")
 		for projectName := range data.Config.P {
+			knownErrors := make([]KnownError, 0)
 			data.SetObj(fmt.Sprintf("knownErrors-%s", projectName), knownErrors, 1*time.Hour)
 		}
 	} else {
@@ -38,13 +39,33 @@ func openSession(data *storage.Data) map[string]*SessionData {
 		}
 	}
 
+	go func(sessionDataMap map[string]*SessionData) {
+		ticker := time.NewTicker(72 * time.Hour)
+		for {
+			<-ticker.C
+			for projectName, sessData := range sessionDataMap {
+				knownErrors := make([]KnownError, 0)
+				sessData.KnownErrorsMutex.Lock()
+				sessionDataMap[projectName].KnownErrors = knownErrors
+				sessData.KnownErrorsMutex.Unlock()
+				data.PSend(projectName, fmt.Sprintf(
+					"Error records have been forcefully cleaned. Next cleaning in 3 days.",
+				))
+			}
+		}
+	}(sessionDataMap)
+
 	go func(sessDataMap map[string]*SessionData) {
 		ticker := time.NewTicker(1 * time.Minute)
 		for {
 			<-ticker.C
 			for projectName, sessData := range sessDataMap {
 				sessData.KnownErrorsMutex.Lock()
-				data.SetObj(fmt.Sprintf("knownErrors-%s", projectName), sessData.KnownErrors, 5*time.Hour) // ttl in case goroutine fails
+				data.SetObj(
+					fmt.Sprintf("knownErrors-%s", projectName),
+					sessData.KnownErrors,
+					5*time.Hour,
+				) // ttl in case goroutine fails
 				sessData.KnownErrorsMutex.Unlock()
 			}
 		}
@@ -57,7 +78,7 @@ func openSession(data *storage.Data) map[string]*SessionData {
 			for _, sessData := range sessDataMap {
 				sessData.KnownErrorsMutex.Lock()
 				for i, knownError := range sessData.KnownErrors {
-					if knownError.LastSeen.Before(time.Now().Add(time.Duration(-8) * time.Hour)) {
+					if knownError.LastSeen.Before(time.Now().Add(time.Duration(-12) * time.Hour)) {
 						sessData.KnownErrors = remove(sessData.KnownErrors, i)
 						log.Println("deteted old error", knownError.OriginBadgerKey)
 						break
