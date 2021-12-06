e59e7cfe777225677ae43c4834b24fb2c371adde
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-06 13:21:07 +0300

    fix(bot): remove sending messages on every text

--- pkg/bot/ontext.go
+++ pkg/bot/ontext.go
@@ -12,14 +12,14 @@ import (
 func registerOnTextHanler(b *tb.Bot, data *storage.Data) {
 	b.Handle(tb.OnText, func(m *tb.Message) {
 		// TODO boss only
+		if m.Sender.Username != data.SysAdmin {
+			data.Send(data.Chat, fmt.Sprintf("@%s, help! Someone is trying to mess with me!", data.SysAdmin))
+			return
+		}
 
 		r, _ := regexp.Compile(`^Set error similarity threshold (0\.\d+)$`)
 		arr := r.FindStringSubmatch(m.Text)
 		if arr != nil {
-			if m.Sender.Username != data.SysAdmin {
-				data.Send(data.Chat, fmt.Sprintf("@%s, help! Someone is trying to mess with me!", data.SysAdmin))
-				return
-			}
 			simStr := arr[1]
 			simFloat, err := strconv.ParseFloat(simStr, 64)
 			if err != nil {
@@ -29,7 +29,5 @@ func registerOnTextHanler(b *tb.Bot, data *storage.Data) {
 			data.Config.Internal.Log.SimilarityThreshold = simFloat
 			data.CSend("New threshold is set.")
 		}
-
-		data.CSend("/help to get list of commands.")
 	})
 }
