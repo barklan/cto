839015d31fbc563a5f258edf0795b2186c1b6531
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-07 16:29:02 +0300

    fix(grpc): static build for manager and direct access to 50051

--- scripts/update_manager_stag.sh
+++ scripts/update_manager_stag.sh
@@ -3,6 +3,9 @@
 # Exit in case of error
 set -e
 
-bash -c "cd ./cmd/manager && go build -o manager ."
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build \
+    -ldflags='-w -s -extldflags "-static"' -a \
+    -o ./cmd/manager/manager ./cmd/manager/.
 
 scp ./cmd/manager/manager stag:/home/ubuntu/stag/manager
