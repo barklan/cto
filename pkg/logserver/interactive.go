45cfa5d634ed5fbf7c1104937dc0e2d97bbafb6b
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-12 00:55:04 +0300

    fix(logrecord): logData will be struct instead of map

--- pkg/logserver/interactive.go
+++ pkg/logserver/interactive.go
@@ -14,9 +14,50 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-func handleErrorRecordInteractive(
+func notifyAboutError(
 	data *storage.Data,
-	logData map[string]string,
+	projectName string,
+	logData *LogMetadata,
+	badgerKey string,
+) {
+	queryString := url.QueryEscape(badgerKey)
+	exactLogURL := fmt.Sprintf(
+		"%s/log/exact?key=%s",
+		data.Config.Internal.Log.ServiceHostname,
+		queryString,
+	)
+	selector := &tb.ReplyMarkup{}
+	btnURL := selector.URL("View Log", exactLogURL)
+	selector.Inline(
+		selector.Row(btnURL),
+	)
+
+	// Get elapsed tiem
+	timeStr := logData.Timestamp
+	var extraTimeStr string
+	fluentdTime, err := time.Parse("2006-01-02 15:04:05 -0700", timeStr)
+	if err == nil {
+		now := time.Now()
+		elapsed := now.Sub(fluentdTime).Round(time.Second)
+		extraTimeStr = fmt.Sprintf(" (%s ago)", elapsed)
+	}
+
+	upperFlag := strings.ToUpper(logData.Flag)
+	message := fmt.Sprintf(
+		"*%s* in %s (%s) at %s%s.",
+		upperFlag,
+		logData.Service,
+		logData.Hostname,
+		timeStr,
+		extraTimeStr,
+	)
+
+	data.PSend(projectName, message, tb.ModeMarkdown, selector)
+}
+
+func handleErrorRecord(
+	data *storage.Data,
+	logData *LogMetadata,
 	record RawLogRecord,
 	badgerKey string,
 	sessData *SessionData,
@@ -55,40 +96,7 @@ func handleErrorRecordInteractive(
 	log.Println("Added new Error!")
 	sessData.KnownErrorsMutex.Unlock()
 
-	// notify
-	queryString := url.QueryEscape(badgerKey)
-	exactLogURL := fmt.Sprintf(
-		"%s/log/exact?key=%s",
-		data.Config.Internal.Log.ServiceHostname,
-		queryString,
-	)
-	selector := &tb.ReplyMarkup{}
-	btnURL := selector.URL("View Log", exactLogURL)
-	selector.Inline(
-		selector.Row(btnURL),
-	)
-
-	// Get elapsed tiem
-	timeStr := logData["fluentd_time"]
-	var extraTimeStr string
-	fluentdTime, err := time.Parse("2006-01-02 15:04:05 -0700", timeStr)
-	if err == nil {
-		now := time.Now()
-		elapsed := now.Sub(fluentdTime).Round(time.Second)
-		extraTimeStr = fmt.Sprintf(" (%s ago)", elapsed)
-	}
-
-	upperFlag := strings.ToUpper(logData["flag"])
-	message := fmt.Sprintf(
-		"*%s* in %s (%s) at %s%s.",
-		upperFlag,
-		logData["service_name"],
-		logData["fluentd_hostname"],
-		timeStr,
-		extraTimeStr,
-	)
-
-	data.PSend(projectName, message, tb.ModeMarkdown, selector)
+	notifyAboutError(data, projectName, logData, badgerKey)
 
 	return nil
 }
--- pkg/logserver/logrecord.go
+++ pkg/logserver/logrecord.go
@@ -25,11 +25,44 @@ type RawLogRecord map[string]interface{}
 
 // TODO add more fields (may be specific to service - handle default values gracefully)
 // TODO is this used anywhere?
-type MetaLogData struct {
-	Hostname    string `json:"fluentd_hostname"`
-	ServiceName string `json:"service_name"`
-	Timestamp   string `json:"fluentd_time"`
-	LogString   string `json:"log"`
+type LogMetadata struct {
+	Hostname  string `json:"fluentd_hostname"`
+	Service   string `json:"service_name"`
+	Timestamp string `json:"fluentd_time"`
+	Flag      string
+}
+
+func constructMetadata(record RawLogRecord) *LogMetadata {
+	logMetadata := LogMetadata{}
+
+	key := "fluentd_hostname"
+	if val, ok := record[key]; ok {
+		if v, okType := val.(string); okType {
+			logMetadata.Hostname = v
+		}
+	} else {
+		log.Printf("Record missing required field: %v", key)
+	}
+
+	key = "service_name"
+	if val, ok := record[key]; ok {
+		if v, okType := val.(string); okType {
+			logMetadata.Service = v
+		}
+	} else {
+		log.Printf("Record missing required field: %v", key)
+	}
+
+	key = "fluentd_time"
+	if val, ok := record[key]; ok {
+		if v, okType := val.(string); okType {
+			logMetadata.Timestamp = v
+		}
+	} else {
+		log.Printf("Record missing required field: %v", key)
+	}
+
+	return &logMetadata
 }
 
 // TODO benchmark this.
@@ -40,37 +73,23 @@ func processLogRecord(
 	// TODO google: passing one-way channels to functions
 	reportChan chan LogRecordReport,
 ) {
-	// TODO logData should be struct instead
-	logData := map[string]string{
-		"service_name":     "",
-		"fluentd_hostname": "",
-		"fluentd_time":     "",
-	}
+	logData := constructMetadata(record)
 
-	for key := range logData {
-		if val, ok := record[key]; ok {
-			logData[key] = fmt.Sprint(val)
-		} else {
-			log.Printf("Record missing required field: %v", key)
-			return
-		}
-	}
-
-	projectName, ok := data.Config.EnvToProjectName[logData["fluentd_hostname"]]
+	projectName, ok := data.Config.EnvToProjectName[logData.Hostname]
 	if !ok {
-		log.Println("Did not find project associated with", logData["fluentd_hostname"])
+		log.Println("Did not find project associated with", logData.Hostname)
 		return
 	}
 
 	// TODO it is heavy to do it on every log record - should decide randomly instead
-	knownServices := querying.GetKnownServices(data, logData["fluentd_hostname"])
-	if _, ok := knownServices[logData["fluentd_hostname"]]; !ok {
-		knownServices[logData["service_name"]] = struct{}{}
-		querying.SetKnownServices(data, logData["fluentd_hostname"], knownServices)
+	knownServices := querying.GetKnownServices(data, logData.Hostname)
+	if _, ok := knownServices[logData.Hostname]; !ok {
+		knownServices[logData.Service] = struct{}{}
+		querying.SetKnownServices(data, logData.Hostname, knownServices)
 	}
 
 	flag := assignFlag(fmt.Sprint(record))
-	logData["flag"] = flag
+	logData.Flag = flag
 
 	// Save log record
 	badgerKey, _ := constructBadgerKey(data, logData, projectName)
@@ -81,9 +100,9 @@ func processLogRecord(
 	// log.Printf("Log added with key: %q", badgerKey)
 
 	// Send message if error
-	if logData["flag"] != flagNone {
+	if logData.Flag != flagNone {
 		projectSessData := sessDataMap[projectName]
-		_ = handleErrorRecordInteractive(data, logData, record, badgerKey, projectSessData, projectName)
+		_ = handleErrorRecord(data, logData, record, badgerKey, projectSessData, projectName)
 	}
 
 	reportChan <- LogRecordReport{ProjectName: projectName}
@@ -91,20 +110,20 @@ func processLogRecord(
 
 func constructBadgerKey(
 	data *storage.Data,
-	logData map[string]string,
+	logData *LogMetadata,
 	projectName string,
 ) (string, error) {
 	randID := strconv.FormatInt(rand.Int63(), 10)
-	fluentdTimeArr := strings.Fields(logData["fluentd_time"])
+	fluentdTimeArr := strings.Fields(logData.Timestamp)
 	dateString, timeString := fluentdTimeArr[0], fluentdTimeArr[1]
 	badgerKey := strings.Join(
 		[]string{
 			projectName,
-			logData["fluentd_hostname"],
-			logData["service_name"],
+			logData.Hostname,
+			logData.Service,
 			dateString,
 			timeString,
-			logData["flag"],
+			logData.Flag,
 			randID,
 		}, " ",
 	)
