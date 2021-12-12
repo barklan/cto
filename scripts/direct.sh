f6bf94448890196b9c93f99053ddac4364501074
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-12 03:41:13 +0300

    chore(direct.sh): ssh server name

--- scripts/direct.sh
+++ scripts/direct.sh
@@ -3,21 +3,22 @@
 set -e
 
 export DOCKER_BUILDKIT=1
+export SSH_SERVER_NAME=cto
 
 docker build -t "barklan/gitlab-workflow-bot:rolling" .
 docker image push "barklan/gitlab-workflow-bot:rolling"
 docker build -t "barklan/cto-explorer:rolling" ./frontend
 docker image push "barklan/cto-explorer:rolling"
 
-ssh -tt -o StrictHostKeyChecking=no "helper" "mkdir -p /home/docker/cto/.cache && mkdir -p /home/docker/cto/environment"
-scp .env "helper:/home/docker/cto"
+ssh -tt -o StrictHostKeyChecking=no "${SSH_SERVER_NAME}" "mkdir -p /home/docker/cto/.cache && mkdir -p /home/docker/cto/environment"
+scp .env "${SSH_SERVER_NAME}:/home/docker/cto"
 
-ssh -tt -o StrictHostKeyChecking=no "helper" \
+ssh -tt -o StrictHostKeyChecking=no "${SSH_SERVER_NAME}" \
 "mkdir -p /home/docker/cto && cd /home/docker/cto && docker-compose down"
-scp docker-compose.yml "helper:/home/docker/cto/"
-scp -r environment "helper:/home/docker/cto"
+scp docker-compose.yml "${SSH_SERVER_NAME}:/home/docker/cto/"
+scp -r environment "${SSH_SERVER_NAME}:/home/docker/cto"
 
-ssh -tt -o StrictHostKeyChecking=no "helper" \
+ssh -tt -o StrictHostKeyChecking=no "${SSH_SERVER_NAME}" \
 "cd /home/docker/cto && \
 docker volume create cto-data && \
 docker volume create cto-media && \
