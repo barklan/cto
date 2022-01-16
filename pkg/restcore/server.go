cc27265099bcdaa245e7525287110d76e50bc2ff
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-01-17 00:13:12 +0300

    feat(core): add handle to delete all keys with specified prefix

--- pkg/restcore/server.go
+++ pkg/restcore/server.go
@@ -1,12 +1,14 @@
 package restcore
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"go.uber.org/zap"
 
 	"github.com/barklan/cto/pkg/storage"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -41,8 +43,36 @@ func Serve(data *storage.Data) {
 func (c *restCore) deletePrefix(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	prefix := r.URL.Query().Get("prefix")
+
+	var count uint64
+	if err := c.Data.DB.Update(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		prefix := []byte(prefix)
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			k := item.Key()
+			err := txn.Delete(k)
+			if err != nil {
+				return err
+			}
+			count++
+		}
+		return nil
+	}); err != nil {
+		c.Data.Log.Error("error when iterating badger keys", zap.Error(err))
+		http.Error(w, "stopped prematurely", http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(prefix))
+	resp := map[string]uint64{"count": count}
+	respJson, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, "failed to marshal", http.StatusInternalServerError)
+	}
+	w.Write(respJson)
 }
 
 func createNewProject(data *storage.Data, w http.ResponseWriter, r *http.Request) {
